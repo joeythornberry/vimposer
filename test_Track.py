64dc9f9793e486549cad23afe60f04cf543a030a
Joey Thornberry <lewis.thornberry@christendom.edu>
2024-06-18 18:38:13 +0000

    initial commit

---
+++ test_Track.py
@@ -0,0 +1,51 @@
+import unittest
+import track
+
+class TestTrack(unittest.TestCase):
+    def setUp(self):
+        self.t = track.Track()
+
+    def test_extend_to(self):
+        self.t = track.Track()
+        self.assertEqual(len(self.t.chords),0)
+        self.t.extend_to(5)
+        self.assertEqual(len(self.t.chords),5)
+        self.t.extend_to(3)
+        self.assertEqual(len(self.t.chords),5)
+        self.t.extend_to(10)
+        self.assertEqual(len(self.t.chords),10)
+
+    def test_add_note(self):
+        self.t = track.Track()
+        self.t.extend_to(5)
+        self.t.add_note(1,2,track.Note(3))
+        for num in [2,3]:
+            self.assertIn(str(num), str(self.t.chords[1]))
+
+class TestChord(unittest.TestCase):
+    def setUp(self):
+        self.c = track.Chord()
+
+    def test_add_note(self):
+        self.c = track.Chord()
+        self.c.add_note(10,track.Note(4))
+        self.c.add_note(5,track.Note(8))
+        self.assertEqual(len(self.c.notes), 2)
+        for num in [10,5,4,8]:
+            self.assertIn(str(num), str(self.c))
+
+    def test_pitch_occupied(self):
+        self.c = track.Chord()
+        self.c.add_note(4, track.Note(4))
+        self.assertTrue(self.c.pitch_occupied(4))
+        self.assertFalse(self.c.pitch_occupied(6))
+
+    def test_remove_note(self):
+        self.c = track.Chord()
+        self.c.add_note(4, track.Note(4))
+        note = self.c.remove_note(4)
+        self.assertFalse(self.c.pitch_occupied(4))
+        self.assertEqual(note.d, 4)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ track.py
@@ -0,0 +1,53 @@
+class Note:
+    def __init__(self, d):
+        self.d = d
+
+    def __repr__(self):
+        return f"<duration: {self.d}>"
+
+class Chord:
+    def __init__(self):
+        self.notes = {}
+
+    def __repr__(self):
+        notestring = ""
+        for key,n in self.notes.items():
+            notestring += f" {key}: {n},"
+        return f"[{notestring}]"
+
+    def pitch_occupied(self, pitch):
+        return pitch in self.notes
+
+    def add_note(self, pitch, note):
+        if(self.pitch_occupied(pitch)):
+           raise Exception(f"Chord Error: pitch {pitch} is occupied already")
+        self.notes[pitch] = note
+
+    def remove_note(self, pitch):
+        if(not self.pitch_occupied(pitch)):
+            raise Exception(f"Chord Error: no note at {pitch} to delete")
+        note = self.notes[pitch]
+        del self.notes[pitch]
+        return note
+
+
+class Track:
+    def __init__(self):
+        self.chords = []
+
+    def __repr__(self):
+        chordstring = ""
+        for c in self.chords:
+            chordstring += f"\t{c}\n"
+        return f"TRACK\n{chordstring}END TRACK"
+
+    def extend_to(self, to):
+        needed_chords = to - len(self.chords)
+        if to > 0:
+            for _ in range(needed_chords):
+                self.chords.append(Chord())
+
+    def add_note(self,x,p,note):
+        if x >= len(self.chords):
+            raise Exception(f"Track Error: chord {x} is out of range")
+        self.chords[x].add_note(p,note)
