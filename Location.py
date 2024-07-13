c5672434d126e2546f1fe6534a56a2b49fc61da5
joeythornberry <joeythornberry@christendom.edu>
2024-07-13 15:28:11 -0400

    [clean] Location.py

--- Location.py
+++ Location.py
@@ -1,7 +1,9 @@
 class Location:
-    p : int
-    x : int
+    """Hold a pair of (p, x) coords."""
+    p: int
+    x: int
 
-    def __init__(self,p:int,x:int):
+    def __init__(self, p: int, x: int):
+        """Init a Location with the specified coords."""
         self.p = p
         self.x = x
