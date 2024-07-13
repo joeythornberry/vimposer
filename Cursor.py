f0c149d9aa4aef033962dfb5598894cd04da2bee
joeythornberry <joeythornberry@christendom.edu>
2024-07-13 18:23:09 -0400

    [clean] Cursor.py

--- Cursor.py
+++ Cursor.py
@@ -1,11 +1,14 @@
 class Cursor:
-    p : int
-    x : int
+    """Store the (p, x) coordinates of the cursored Note."""
+    p: int
+    x: int
 
-    def __init__(self,p,x):
+    def __init__(self, p: int, x: int):
+        """Init a new Cursor with the given coords."""
         self.p = p
         self.x = x
 
-    def set(self,p,x):
+    def set(self, p: int, x: int): 
+        """Set the Cursor's coords to the given values."""
         self.p = p
         self.x = x
