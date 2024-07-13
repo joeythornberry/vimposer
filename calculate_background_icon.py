8bbcf63105e82c4dd0449f2ab9702cf7961efc3b
joeythornberry <joeythornberry@christendom.edu>
2024-07-13 18:26:41 -0400

    [clean] calculate_background_icon.py

--- calculate_background_icon.py
+++ calculate_background_icon.py
@@ -1,4 +1,5 @@
-def calculate_background_icon(p,x):
+def calculate_background_icon(p: int, x: int) -> str:
+    """Returns the icon that the given location should display if it has no notes on it."""
     notes = "c#d#ef#g#a#b"
     i = notes[p % 12]
     if x % 24 == 23:
