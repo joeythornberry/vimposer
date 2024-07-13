3f7435a29867cb5975acddc9ec59ee6b66d76c5c
joeythornberry <joeythornberry@christendom.edu>
2024-07-13 17:15:15 -0400

    [clean] Chord.py

--- Chord.py
+++ Chord.py
@@ -1,7 +1,10 @@
 from Note import Note
 
 class Chord:
+    """Store all of the Notes for a given x-value."""
+
     def __init__(self):
+        """Init Chord with empty notes dict."""
         self.notes : dict[int,Note] = {}
 
     def __repr__(self):
@@ -10,27 +13,31 @@ class Chord:
             notestring += f" {key}: {n},"
         return f"[{notestring}]"
 
-    def pitch_occupied(self, pitch):
-        return pitch in self.notes
+    def pitch_occupied(self, p: int) -> bool:
+        """Return True if this chord contains a note at the given pitch"""
+        return p in self.notes
 
-    def add_note(self, pitch, note):
-        if(self.pitch_occupied(pitch)):
-           raise Exception(f"Chord Error: pitch {pitch} is occupied already")
-        self.notes[pitch] = note
+    def add_note(self, p: int, note: Note):
+        """Add note to this chord at the given pitch. Throw error if pitch is already occupied."""
+        if(self.pitch_occupied(p)):
+           raise Exception(f"Chord Error: pitch {p} is occupied already")
+        self.notes[p] = note
 
-    def remove_note(self, pitch):
-        if(not self.pitch_occupied(pitch)):
-            raise Exception(f"Chord Error: no note at {pitch} to delete")
-        note = self.notes[pitch]
-        del self.notes[pitch]
-        return note
+    def remove_note(self, p: int):
+        """Remove the Note at the given pitch from this chord. Throw error if there is no Note at the given pitch."""
+        if(not self.pitch_occupied(p)):
+            raise Exception(f"Chord Error: no note at {p} to delete")
+        del self.notes[p]
 
     def has_more_than_one_note(self):
+        """Return True if there is more than one Note in this chord.""" 
         return len(self.notes.keys()) > 1
 
-    def get_notes_list(self) -> list[tuple[int,Note]]:
+    def get_notes_list(self) -> list[tuple[int, Note]]:
+        """Return this chord's pitch-Note combinations as a list of (p, Note) tuples."""
         return list(self.notes.items())
 
-    def set_note_length(self,p,l):
+    def set_note_length(self, p: int,l: int):
+        """Set the length of the note at pitch p in this chord to l."""
         self.notes[p].l = l
 
