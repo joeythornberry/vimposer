65a3f0c27fa8b43b0cafa7d9bcdeb92cbd6e1dfe
joeythornberry <joeythornberry@christendom.edu>
2024-07-13 16:53:47 -0400

    [clean] Note.py

--- Note.py
+++ Note.py
@@ -1,6 +1,11 @@
 class Note:
+    """Represent a MIDI note. 
+
+    l -- The length of the note.
+    """
     l : int
     def __init__(self, l : int):
+        """Init a Note with the given length."""
         self.l = l
 
     def __repr__(self):
