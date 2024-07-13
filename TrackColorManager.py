66bc88dee13c5d16a0d131e056abf65182ec48d8
joeythornberry <joeythornberry@christendom.edu>
2024-07-13 18:19:07 -0400

    [clean] TrackColorManager.py

--- TrackColorManager.py
+++ TrackColorManager.py
@@ -1,24 +1,24 @@
 class TrackColorManager:
-    track_to_color_map : dict[int,int]
+    """Handle a map of track ids to color ids, so we know what color each track should be."""
+    track_to_color_map: dict[int, int]
 
     def __init__(self):
-        self.track_to_color_map = {}
+        """Init a TrackColorManager with an empty track-to-color dict."""
+        self.track_to_color_map: dict[int, int] = {}
 
-    def assign_track_color(self, t : int):
-        if not self.track_to_color_map:
-            self.track_to_color_map[t] = 0
-            return 0
+    def assign_track_color(self, track: int):
+        """Generate a new color id and assign it to the given track."""
         taken_colors = list(self.track_to_color_map.values())
-
-        color = 0
+        color = 0 # just need to find a color id that isn't taken already
         while color in taken_colors:
             color += 1
 
-        self.track_to_color_map[t] = color
-        return color
+        self.track_to_color_map[track] = color
 
-    def free_track_color(self, t : int):
-        del self.track_to_color_map[t]
+    def free_track_color(self, track: int):
+        """Delete a track-color mapping from the record."""
+        del self.track_to_color_map[track]
 
-    def get_track_color(self, t : int) -> int:
-        return self.track_to_color_map[t]
+    def get_track_color(self, track: int) -> int:
+        """Return the color id that the given track is associated with."""
+        return self.track_to_color_map[track]
