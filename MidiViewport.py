02d9c109f66cf4589f994b3e207a63182532b8ac
joeythornberry <joeythornberry@christendom.edu>
2024-07-13 14:28:02 -0400

    [clean] MidiViewport.py

--- MidiViewport.py
+++ MidiViewport.py
@@ -1,5 +1,5 @@
 class MidiViewport:
-    """Defines area of screen dedicated to midi, and translates (p,x) to (line,char) coords.
+    """Define area of screen dedicated to midi, and translate (p,x) to (line,char) coords.
 
     Attributes:
     across -- How far right the viewport has been shifted (never negative)
@@ -18,59 +18,79 @@ class MidiViewport:
     height: int
 
     def __init__(self):
+        """Init a MidiViewport with across and up offsets set to 0"""
         self.across = 0
         self.up = 0
 
-    def set_dimensions(self,top,bottom,left,right):
+    def set_dimensions(self, top: int, bottom: int, left: int, right: int):
+        """Set viewport borders to given values, and calculate viewport height"""
         self.top = top
         self.bottom = bottom
         self.left = left
         self.right = right 
         self.height = self.bottom - self.top
 
-    def shift_across(self,amount):
+    def shift_across(self, amount: int):
+        """Shift viewport across the screen.
+
+        Can shift in negative direction, but will not set across to a negative value.
+        """
         if self.across == 0 and amount < 0:
             return
         self.across += amount
 
-    def shift_up(self,amount):
+    def shift_up(self, amount: int):
+        """Shift viewport up and down the screen.
+
+        Can shift in negative direction, but will not set up to a negative value, or to a value that would 
+        draw a pitch location above 127 on the screen.
+        """
         if (self.up == 0 and amount < 0) or (self.up == (129-self.height) and amount > 0):
             return
         self.up += amount
 
-    def translate_coords(self,p,x):
+    def translate_coords(self, p: int, x: int) -> tuple[int, int, bool]:
+        """Translate (p, x) coords to (line, char) coords for display on screen, and report if coords will be in viewport or not.
+
+        Returns line, char, onscreen, where onscreen is True if the coordinates will be in the viewport and should be drawn.
+        """
         line = self.top + self.up + self.height - p - 1
         char = x + self.left - self.across + 1
         onscreen = not (line < self.top + 1 or line > self.bottom - 1 or char < self.left + 1 or char > self.right - 1)
-        return line,char,onscreen
+        return line, char, onscreen
 
-    def translate_coords_reverse(self,line,char):
+    def translate_coords_reverse(self,line,char) -> tuple[int, int]:
+        """Translate (line, char) coords to (p, x) coords."""
         p = self.top + self.up + self.height - line - 1
         x = char - self.left + self.across - 1
-        return p,x
+        return p, x
 
     def locate_full_screen(self):
+        """Yield all (p, x) coordinate pairs that are currently visible on the screen"""
         for y in range(self.top,self.bottom):
             for x in range(self.left,self.right):
                 yield self.translate_coords_reverse(y,x)
 
     def yield_border(self):
+        """Yield (line, char) coords and ui icon types for the viewport's border on the screen.
 
+        Yields line, char, icon.
+        """
         # top
-        for x in range(self.left+1, self.right):
-            yield self.top,x,"top_border"
+        for char in range(self.left+1, self.right):
+            yield self.top, char, "top_border"
 
         # bottom
-        for x in range(self.left+1, self.right):
-            yield self.bottom,x,"bottom_border"
+        for char in range(self.left+1, self.right):
+            yield self.bottom, char, "bottom_border"
 
         # left
-        for y in range(self.top+1, self.bottom):
-            yield y, self.left, "left_border"
+        for line in range(self.top+1, self.bottom):
+            yield line, self.left, "left_border"
 
         # right 
-        for y in range(self.top+1, self.bottom):
-            yield y, self.right, "right_border"
+        for line in range(self.top+1, self.bottom):
+            yield line, self.right, "right_border"
 
         yield self.top,self.left,"top_left_corner"
         yield self.top,self.right,"top_right_corner"
