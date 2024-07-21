33312806b512faafc62b26349089a0815af2e39c
joeythornberry <joeythornberry@christendom.edu>
2024-07-21 15:12:55 -0400

    delete outdated tests

--- tests/quantized_delta_times.py
+++
@@ -1,8 +0,0 @@
-from vimposerparsing.MidiFileParseResult import MidiFileParseResult
-from vimposerparsing.parse_midi_file import parse_midi_file
-
-midi_file_parse_result: MidiFileParseResult = parse_midi_file("MIDI/quantized_delta_times.mid", 100)
-print(midi_file_parse_result.delta_times)
-assert(480 in midi_file_parse_result.delta_times) # quarter note is picked up
-assert(960 not in midi_file_parse_result.delta_times) # half note is avoided as multiple
-assert(1920 not in midi_file_parse_result.delta_times) # whole note gap is avoided as multiple
--- tests/unquantized_delta_times.py
+++
@@ -1,7 +0,0 @@
-from vimposerparsing.MidiFileParseResult import MidiFileParseResult
-from vimposerparsing.parse_midi_file import parse_midi_file
-
-midi_file_parse_result: MidiFileParseResult = parse_midi_file("MIDI/unquantized_delta_times.mid", 200)
-print(midi_file_parse_result.delta_times)
-assert(480 in midi_file_parse_result.delta_times) # quarter note is picked up
-assert(1790 not in midi_file_parse_result.delta_times) # short whole note gap is ignored as multiple
