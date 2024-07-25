4fab53948a28449c2541d4df954ed0f6059e10bb
joeythornberry <joeythornberry@christendom.edu>
2024-07-24 21:29:34 -0400

    fix bin_repr

--- c/midiloader.c
+++ c/midiloader.c
@@ -5,23 +5,16 @@
 
 char * wchar_t_to_const_char(wchar_t * wchar_t_string) {
 	size_t len = wcslen(wchar_t_string);
-	printf("%ld\n", len);
 	char * char_string = malloc(len + 1);
-	printf("mallocced successfully\n");
 	for (uint8_t i = 0; i < len; i++) {
-		char c = wchar_t_string[i];
-		printf("char: %c\n",c);
-		char_string[i] = c;
+		char_string[i] = wchar_t_string[i];
 	}
 	char_string[len] = '\0';
-	printf("%s\n", char_string);
-
 	return char_string;
 }
 
 const char read_bit(uint8_t input, uint8_t bit_to_read) {
-	uint8_t bit = input && (1 << bit_to_read);
-	printf("%d\n", input);
+	uint8_t bit = input & (1 << bit_to_read);
 	if (bit == 0) {
 		return '0';
 	} else {
@@ -30,16 +23,13 @@ const char read_bit(uint8_t input, uint8_t bit_to_read) {
 }
 
 void binary_repr(char dest[8], uint8_t input) {
-	printf("input: %d\n", input);
 	for(uint8_t i = 0; i < 8; i++) {
 		dest[i] = read_bit(input, i);
 	}
 }
 
 const char * parse_midi_file(wchar_t * filename_wchar_p, int (*save_note)(int, int, int, int)) {
-	printf("Opening %ls\n", filename_wchar_p);
-	const char filename_const_char = (const char) * filename_wchar_p;
-	printf("cast attempt: %s\n", &filename_const_char);
+
 	char * filename = wchar_t_to_const_char(filename_wchar_p);
 	printf("conversion func attempt: %s\n", filename);
 
